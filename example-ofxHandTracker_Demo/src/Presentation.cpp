0613b6eb5aaa2068a363f20132f5c4cf745fe67f
rusthigh <rusthigh@proton.me>
2021-03-20 15:59:41 +0000

    Second SHT i2c Sensor (SHT3x or SHT85)

--- example-ofxHandTracker_Demo/src/Presentation.cpp
+++ example-ofxHandTracker_Demo/src/Presentation.cpp
@@ -12,4 +12,16 @@ Presentation::Presentation() {
     setup();
 }
 
-Presentation::Presentation
+Presentation::Presentation(string _assetsFilename, string _menusFilename, string _vizualType) {
+    setup();
+}
+ 
+void Presentation::setup() {
+
+    images.clear();
+        
+    int sumWidth = 0;
+    int maxHeight = 0;
+    int maxWidth = 0;
+
+	ofDirectory dir = ofDi
