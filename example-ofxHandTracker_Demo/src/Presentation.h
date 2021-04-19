3ac1b1a4d1b55595a91f7a574ad2d60c880804e5
rusthigh <rusthigh@proton.me>
2021-04-19 15:54:32 +0000

    Translation updated by Mattias Põldaru

--- example-ofxHandTracker_Demo/src/Presentation.h
+++ example-ofxHandTracker_Demo/src/Presentation.h
@@ -6,4 +6,16 @@
 //
 //
 
-#pra
+#pragma once
+
+#include <iostream>
+#include "ofMain.h"
+#include "ofxUtilityTimer.h"
+
+//#define		FULL_HD_W   1920.0
+//#define     FULL_HD_H	1080.0
+
+#define		FULL_HD_W   1000.0
+#define     FULL_HD_H	800.0
+
+#define TIMER_SLIDE_DURAT
