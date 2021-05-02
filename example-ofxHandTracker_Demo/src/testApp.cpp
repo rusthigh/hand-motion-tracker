f677a048fdef80bd55878bb3f35667503e00f588
rusthigh <rusthigh@proton.me>
2021-05-02 10:37:34 +0000

    Update navbar.php

--- example-ofxHandTracker_Demo/src/testApp.cpp
+++ example-ofxHandTracker_Demo/src/testApp.cpp
@@ -1,3 +1,10 @@
 #include "testApp.h"
 
-//---------------------------------
+//--------------------------------------------------------------
+void testApp::setup(){
+
+	for(int i=0; i<256; i++)
+		activeKeys[i] = false;
+
+	ofSetBackgroundAuto(false); // removes fullscreen flickering (on some PCs)
+	ofBack
