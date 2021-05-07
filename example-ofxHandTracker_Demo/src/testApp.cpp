30399d4652c54b399c27b05fc776e141e8227a48
rusthigh <rusthigh@proton.me>
2021-05-07 11:13:42 +0000

    hrtimer: Allow concurrent hrtimer_start() for self restarting timers

--- example-ofxHandTracker_Demo/src/testApp.cpp
+++ example-ofxHandTracker_Demo/src/testApp.cpp
@@ -30,4 +30,12 @@ void testApp::setup(){
 	userGen.setSmoothing(0.1);				// built in openni skeleton smoothing...
 	//userGen.setUseMaskPixels(true);
 	userGen.setUseCloudPoints(true); // must be enabled to be able to draw cloud points
-	userGen.setMaxNumberOfUsers(2);					// use this to set dynamic max number of users (NB: that a hard upper limit is d
+	userGen.setMaxNumberOfUsers(2);					// use this to set dynamic max number of users (NB: that a hard upper limit is defined by MAX_NUMBER_USERS in ofxUserGenerator)
+	
+	handGen.setup(&oniContext, 2);	
+	handGen.setSmoothing(0.02);
+
+	oniContext.toggleRegisterViewport();
+	oniContext.toggleMirror();
+
+	tracker = new ofx
