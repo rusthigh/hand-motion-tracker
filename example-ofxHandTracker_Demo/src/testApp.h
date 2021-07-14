c54e064add5e60e450c89d4f4bdbaa1a3674605e
rusthigh <rusthigh@proton.me>
2021-07-14 18:15:31 +0000

    fix js bug

--- example-ofxHandTracker_Demo/src/testApp.h
+++ example-ofxHandTracker_Demo/src/testApp.h
@@ -3,4 +3,13 @@
 
 #pragma once
 
-#in
+#include "ofMain.h"
+#include "ofxHandModel.h"
+#include "ofxHandTracker.h"
+#include "ofxOpenNI.h"
+#include "ofxOpenCv.h"
+#include "Presentation.h"
+
+#define USE_KINECT
+#define NUM_DEMO_PARAMS_X		6
+#define NUM_DE
