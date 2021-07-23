a458f931500fba3fb277cd0688c959003cd021a9
rusthigh <rusthigh@proton.me>
2021-07-23 22:15:20 +0000

    Removed some compiler warnings

--- example-ofxHandTracker_Demo/src/testApp.h
+++ example-ofxHandTracker_Demo/src/testApp.h
@@ -12,4 +12,16 @@
 
 #define USE_KINECT
 #define NUM_DEMO_PARAMS_X		6
-#define NUM_DE
+#define NUM_DEMO_PARAMS_Y		4
+
+class testApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed  (int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int
