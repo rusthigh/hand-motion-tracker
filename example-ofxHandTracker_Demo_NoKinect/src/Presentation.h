b12260a8c51d554c787084fe6806221fe8ef8c93
rusthigh <rusthigh@proton.me>
2022-08-28 05:59:45 +0000

    fixes #12

--- example-ofxHandTracker_Demo_NoKinect/src/Presentation.h
+++ example-ofxHandTracker_Demo_NoKinect/src/Presentation.h
@@ -9,4 +9,14 @@
 #pragma once
 
 #include <iostream>
-#include "ofMa
+#include "ofMain.h"
+#include "ofxUtilityTimer.h"
+
+//#define		FULL_HD_W   1920.0
+//#define     FULL_HD_H	1080.0
+
+#define		FULL_HD_W   1000.0
+#define     FULL_HD_H	800.0
+
+#define TIMER_SLIDE_DURATION	60000
+#define TIM
