235d3773eec30db3bb8eb896aeb1172b821fde07
rusthigh <rusthigh@proton.me>
2022-09-11 14:17:48 +0000

    Fix gitignore

--- example-ofxHandTracker_Demo_NoKinect/src/testApp.h
+++ example-ofxHandTracker_Demo_NoKinect/src/testApp.h
@@ -4,3 +4,11 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxHandModel.h"
+// these are not needed in this example
+//#include "ofxHandTracker.h"
+//#include "ofxOpenNI.h"
+//#include "ofxOpenCv.h"
+#include "Presentation.h"
+
+class testApp : public o
