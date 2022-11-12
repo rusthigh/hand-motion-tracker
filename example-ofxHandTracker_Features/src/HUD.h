80f2cfb7a78abb1166348940268890fb621284f5
rusthigh <rusthigh@proton.me>
2022-11-12 15:07:26 +0000

    fsdfs

--- example-ofxHandTracker_Features/src/HUD.h
+++ example-ofxHandTracker_Features/src/HUD.h
@@ -6,4 +6,6 @@ class HUD
 {
 public:
 	HUD(void);
-	HUD
+	HUD(ofPoint _mapPos, ofColor _mapColor, int _w, int _h);
+	HUD(ofPoint _mapPos, int _mapWidth, int _mapHeight, float _wPages, float _hPages, bool _lockDim = false);
+	HUD(ofPoint _mapPos, int _mapWidth, int _mapHeight, in
