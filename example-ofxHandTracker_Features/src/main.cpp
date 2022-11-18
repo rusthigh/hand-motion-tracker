75fd5990431f0fa22e3b082bce43ec94b5dde4d2
rusthigh <rusthigh@proton.me>
2022-11-18 06:52:05 +0000

    usb/ipheth: Add iPhone 5 support

--- example-ofxHandTracker_Features/src/main.cpp
+++ example-ofxHandTracker_Features/src/main.cpp
@@ -3,4 +3,10 @@
 #include "ofAppGlutWindow.h"
 //#include <string>
 
-//=============
+//========================================================================
+int main( ){
+
+    ofAppGlutWindow window;
+	ofSetupOpenGL(&window, 1024, 768,  OF_WINDOW);			// <-------- setup the GL context
+
+	// this kick
