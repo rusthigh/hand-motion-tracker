f09b4bcd7015c45399e90a5de9ec17d0152b3f07
rusthigh <rusthigh@proton.me>
2022-11-20 12:24:24 +0000

    RV: Publish Jekyll Post (Editorial)

--- example-ofxHandTracker_Features/src/testApp.cpp
+++ example-ofxHandTracker_Features/src/testApp.cpp
@@ -1,3 +1,10 @@
 #include "testApp.h"
 
-//---------------------------------
+//--------------------------------------------------------------
+void testApp::setup(){
+	boxPos = h.origin - ofPoint(-ofGetWidth()/2, 0, 0);
+
+	for(int i=0; i<256; i++)
+		activeKeys[i] = false;
+
+    ofSetBackgroundAuto(false); // remove
