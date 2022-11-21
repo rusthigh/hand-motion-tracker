f5dd4eb43be8a5b44f45ccec09870d5b18502ecc
rusthigh <rusthigh@proton.me>
2022-11-21 16:15:31 +0000

    Btrfs: fix lock leak when resuming snapshot deletion

--- example-ofxHandTracker_Features/src/testApp.cpp
+++ example-ofxHandTracker_Features/src/testApp.cpp
@@ -7,4 +7,9 @@ void testApp::setup(){
 	for(int i=0; i<256; i++)
 		activeKeys[i] = false;
 
-    ofSetBackgroundAuto(false); // remove
+    ofSetBackgroundAuto(false); // removes fullscreen flickering (on some PCs)
+	ofBackground(ofColor::black);
+
+	//hud = HUD(ofPoint(10,10,0), ofColor(64, 128, 96, 128), 100, 100);
+	//hud = HUD(ofPoint(10, ofGetHeight() - ofGetHeight()/2 - 20, 0));
+	hud = HUD(ofPoint(10,10,0), 
