775c7a19b297238d44a457a410954ed55adfdc95
rusthigh <rusthigh@proton.me>
2022-11-24 20:10:19 +0000

    db changes

--- example-ofxHandTracker_Features/src/testApp.cpp
+++ example-ofxHandTracker_Features/src/testApp.cpp
@@ -12,4 +12,5 @@ void testApp::setup(){
 
 	//hud = HUD(ofPoint(10,10,0), ofColor(64, 128, 96, 128), 100, 100);
 	//hud = HUD(ofPoint(10, ofGetHeight() - ofGetHeight()/2 - 20, 0));
-	hud = HUD(ofPoint(10,10,0), 
+	hud = HUD(ofPoint(10,10,0), 120, 60, 2.0f, 2.0f); // init hud with number of pages - always locks number of pages (no matter of resolution)
+	//hud = HUD(ofPoint(10, 870, 0), 250, 150, 2000, 7000); // init hud with locked
