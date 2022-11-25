7d2c5663bba6bf8e5938daf8eb5c4d3884413d11
rusthigh <rusthigh@proton.me>
2022-11-25 11:04:23 +0000

    Logo

--- example-ofxHandTracker_Features/src/testApp.cpp
+++ example-ofxHandTracker_Features/src/testApp.cpp
@@ -13,4 +13,7 @@ void testApp::setup(){
 	//hud = HUD(ofPoint(10,10,0), ofColor(64, 128, 96, 128), 100, 100);
 	//hud = HUD(ofPoint(10, ofGetHeight() - ofGetHeight()/2 - 20, 0));
 	hud = HUD(ofPoint(10,10,0), 120, 60, 2.0f, 2.0f); // init hud with number of pages - always locks number of pages (no matter of resolution)
-	//hud = HUD(ofPoint(10, 870, 0), 250, 150, 2000, 7000); // init hud with locked
+	//hud = HUD(ofPoint(10, 870, 0), 250, 150, 2000, 7000); // init hud with locked resolution to be available (you want 10k x 8k res or even more? fine, just set it up)
+	//hud = HUD(ofPoint(0, 0, 0), 100, 1000, 1000, 10000); // setup for ppt
+	//hud = HUD(ofPoint(0, 0, 0), 1, 1, 1.0f, 1.0f);
+	hud.se
