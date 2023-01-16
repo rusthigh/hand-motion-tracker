820ca29d3f24aabec3ab5a6827781eb2d2318a6b
rusthigh <rusthigh@proton.me>
2023-01-16 20:06:08 +0000

    ext4: don't try to modify s_flags if the the file system is read-only

--- example-ofxHandTracker_Features/src/testApp.h
+++ example-ofxHandTracker_Features/src/testApp.h
@@ -21,4 +21,9 @@ class testApp : public ofBaseApp{
 		void update();
 		void draw();
 
-		void ke
+		void keyPressed  (int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, 
