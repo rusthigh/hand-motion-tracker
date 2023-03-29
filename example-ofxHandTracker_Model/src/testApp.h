79af896454c4d1ac2579f06cfffc1e2d1ddb61d7
rusthigh <rusthigh@proton.me>
2023-03-29 20:51:19 +0000

    v0.9.1

--- example-ofxHandTracker_Model/src/testApp.h
+++ example-ofxHandTracker_Model/src/testApp.h
@@ -6,4 +6,12 @@
 #include "ofMain.h"
 #include "ofxHandModel.h"
 // these are not needed in this example
-//#include "ofxHandTracker.h" // ofxHandTracker in this 
+//#include "ofxHandTracker.h" // ofxHandTracker in this project was also excluded from addons,
+							  // cause ofxOpenNI and ofxOpenCV were not loaded
+
+#define NUM_DEMO_PARAMS_X		6
+#define NUM_DEMO_PARAMS_Y		4
+
+class testApp : public ofBaseApp{
+
+	pub
