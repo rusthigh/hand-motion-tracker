de861e9f98220429ea2e630f98c2950fc19efdac
rusthigh <rusthigh@proton.me>
2023-03-29 06:07:31 +0000

    roselyn broke it

--- example-ofxHandTracker_Model/src/testApp.h
+++ example-ofxHandTracker_Model/src/testApp.h
@@ -1,2 +1,9 @@
 // @author: blazm
-// @about: this example works witho
+// @about: this example works without kinect, just to see kinematic hand model in action 
+
+#pragma once
+
+#include "ofMain.h"
+#include "ofxHandModel.h"
+// these are not needed in this example
+//#include "ofxHandTracker.h" // ofxHandTracker in this 
