052339b548680ccab879a3203a34a2d5a637b3e7
rusthigh <rusthigh@proton.me>
2023-03-30 13:32:31 +0000

    Clean Up & Documentation

--- example-ofxHandTracker_Model/src/testApp.h
+++ example-ofxHandTracker_Model/src/testApp.h
@@ -14,4 +14,13 @@
 
 class testApp : public ofBaseApp{
 
-	pub
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed  (int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mou
