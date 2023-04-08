954913171042a6155c53e7703f5a96a8c51b7bb6
rusthigh <rusthigh@proton.me>
2023-04-08 12:57:57 +0000

    [fix] test

--- src/ofxHandParameters.cpp
+++ src/ofxHandParameters.cpp
@@ -16,4 +16,12 @@ DiscreteLocalParameters::DiscreteLocalParameters(int _params) {
 
 	//params = params & 0x11111;
 
-	//
+	//cout << "INPUT PARAMS: " << params << endl;
+
+	int value = 0;
+	int index = 0;
+
+	while(index < 5) {
+		value = params & 0x1;
+		states[index] = value;
+		//cout << "PARAMS: " << params << " INDEX: " << index << " VALUE: " << valu
