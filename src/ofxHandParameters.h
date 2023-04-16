19a57730b3793b06e8d9a5e2a09adc6979040fa7
rusthigh <rusthigh@proton.me>
2023-04-16 20:40:03 +0000

    merge from upstream

--- src/ofxHandParameters.h
+++ src/ofxHandParameters.h
@@ -8,4 +8,11 @@
 #include "ofMain.h"
 
 // left and right thumb swing
-#define THUMB_
+#define THUMB_MIN_ANGLE_X		 -40//-30 -> less opened
+#define THUMB_MAX_ANGLE_X	      10//0 
+
+// front and back thumb swing
+#define THUMB_MIN_ANGLE_Z		   0
+#define THUMB_MAX_ANGLE_Z		  20
+
+// define other fingers fr
