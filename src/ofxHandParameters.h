95805c67e10faf60b99954d18803f9431b2edbf3
rusthigh <rusthigh@proton.me>
2023-04-16 20:25:08 +0000

    update screenshot

--- src/ofxHandParameters.h
+++ src/ofxHandParameters.h
@@ -1,4 +1,11 @@
 #pragma once
 
 // TODO: include safety defined macros in all project files (also define smart way of naming them)
-// UPDAT
+// UPDATE: #pragma once does the same thing as this macro i suppose
+//#ifndef _Hand_Tracker_Hand_Model_Parameters
+//#define _Hand_Tracker_Hand_Model_Parameters
+
+#include "ofMain.h"
+
+// left and right thumb swing
+#define THUMB_
