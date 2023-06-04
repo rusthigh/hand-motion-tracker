90d0eea41abd710f107a7b049fde894f83bba51b
rusthigh <rusthigh@proton.me>
2023-06-04 21:57:04 +0000

    Add warning to README

--- src/ofxHandTracker.h
+++ src/ofxHandTracker.h
@@ -2,4 +2,13 @@
 
 //#include "ofMain.h"
 #include "ofxHandModel.h"
-#include "ofxOpenN
+#include "ofxOpenNI.h"
+#include "ofxOpenCv.h"
+#include "ofxImageMatcher.h"
+
+#define	TINY_IMG_DIM		150
+#define FTIP_HIST_SIZE		3	// history array size
+
+#define	HAND_GRABBED			"hand_grabbed"
+#define HAND_RELEASED			"hand_released"
+#define HAND_PICK
