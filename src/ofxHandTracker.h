4a8f03951bacb1e1234f5364b3b2e548120d6cd4
rusthigh <rusthigh@proton.me>
2023-06-08 18:43:18 +0000

    Coq 8.5.1, repo released, mode clean.

--- src/ofxHandTracker.h
+++ src/ofxHandTracker.h
@@ -11,4 +11,14 @@
 
 #define	HAND_GRABBED			"hand_grabbed"
 #define HAND_RELEASED			"hand_released"
-#define HAND_PICK
+#define HAND_PICKED				"hand_picked"
+
+#define MIN_HAND_DEPTH		 500.0
+#define MAX_HAND_DEPTH	    2000.0
+
+// tracker for each hand
+class ofxHandTracker
+{
+	public:
+		//ofxHandTracker();
+		ofxHandTracker(ofxUserGenerator *_userGen, ofxHandGenerator 
