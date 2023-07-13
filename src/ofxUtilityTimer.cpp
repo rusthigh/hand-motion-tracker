b74d79e8423df023927f42914f7d66e9c2ea8a10
rusthigh <rusthigh@proton.me>
2023-07-13 20:47:34 +0000

    change config

--- src/ofxUtilityTimer.cpp
+++ src/ofxUtilityTimer.cpp
@@ -54,3 +54,65 @@ void ofxUtilityTimer::start(float millis)
 
 void ofxUtilityTimer::stop()
 {
+    isCounting = false;
+        
+    minutes=0; seconds=0; miliseconds=0;
+    durationTime=0; step=0;
+	durationMillis = 0;
+}
+
+void ofxUtilityTimer::draw(float _x, float _y) {
+	// draw counting up
+	//ofDrawBitmapString(millisToString(getElapsed()), _x, _y, 0);
+	// draw countdown
+	ofDrawBitmapString(millisToString(getDuration() - getElapsed()), _x, _y, 0);
+}
+
+void ofxUtilityTimer::draw(float _x, float _y, float _w, float _h, ofColor _color) {
+	// draw counting up
+	//ofDrawBitmapString(millisToString(getElapsed()), _x, _y, 0);
+	// draw countdown
+	//ofDrawBitmapString(millisToString(getDuration() - getElapsed()), _x, _y, 0);
+
+	ofPushStyle();
+	ofSetColor(_color);
+	ofRect(_x, _y, getPercent() * _w, _h);
+	ofPopStyle();
+}
+
+string ofxUtilityTimer::millisToString(int millis) {
+    int timerMin, timerSec, timerMil;
+    timerMin = millis / 1000 / 60;
+    timerSec = (millis / 1000) % 60;
+    timerMil = (millis % 1000) / 10;
+
+    string timeMessage = ofToString(timerMin) + ":" + (timerSec<10?"0":"") +
+                         ofToString(timerSec) + ":" + (timerMil<10?"0":"") +
+                         ofToString(timerMil);
+    
+    return timeMessage;
+}
+
+float ofxUtilityTimer::getDuration()
+{
+    return durationMillis;
+}
+
+//get starting timer delay
+float ofxUtilityTimer::getStartDuration()
+{
+    return durationTime;
+}
+
+float ofxUtilityTimer::getPercent()
+{
+    return elapsedMillis / durationMillis;
+}
+
+float ofxUtilityTimer::getElapsed() {
+	return elapsedMillis;
+}
+
+ofxUtilityTimer::~ofxUtilityTimer(void)
+{
+}
---
+++ src/ofxUtilityTimer.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "ofMain.h"
+#include <string>
+
+class ofxUtilityTimer
+{
+	public:
+        ofxUtilityTimer(void);
+		~ofxUtil
